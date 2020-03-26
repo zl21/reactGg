448dcc714db03df76aa76698af4d5f81f909847e
zl21 <44433851+zl21@users.noreply.github.com>
2020-03-26 12:55:50 +0800

    Test antd.

--- commodity/src/App.js
+++ commodity/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import {Button} from 'antd'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         this is App component.
+        <Button type='primary'>test antd</Button>
       </div>
     )
   }
