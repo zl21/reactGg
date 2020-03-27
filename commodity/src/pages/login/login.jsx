1ccd6f358ea2c8b71723ea725ae0c3673686374c
zl21 <44433851+zl21@users.noreply.github.com>
2020-03-27 23:35:42 +0800

    axios 2、3.
    使用axios请求拦截器统一处理参数编码问题。
    使用axios响应拦截器过滤数据。

--- commodity/src/pages/login/login.jsx
+++ commodity/src/pages/login/login.jsx
@@ -1,21 +1,59 @@
 import React, { Component } from 'react'
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, message as msg } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import axios from 'axios'
+import qs from 'querystring'
 import './css/login.less'
 import logo from './imgs/logo.png'
 
-const {Item} = Form
+const { Item } = Form
+
+// 配置超时时间
+axios.defaults.timeout = 2000;
+
+/* 
+  使用axios请求拦截器
+  参数:config，里面包含了本次请求所有的配置项（请求地址/方式/参数及参数格式）
+*/
+axios.interceptors.request.use((config) => {
+  // console.log(config);
+  // 如果发的是post请求，并且携带的是json格式的参数/数据，就要将数据处理一下
+  // 获取请求方式及参数
+  const { method, data } = config;
+  if (method.toLowerCase() === 'post' && data instanceof Object) {
+    config.data = qs.stringify(data);
+  }
+  return config;
+})
+
+/* 
+  axios的响应拦截器
+*/
+axios.interceptors.response.use(
+  // 响应成功的回调--状态码2开头
+  response => response.data,
+  err => {
+    // console.log(err);
+    let errmsg = '';
+    const { message } = err;
+    if (message.indexOf('401') !== -1) errmsg = '身份校验失败，请重新登录！';
+    else if (message.indexOf('Network Error') !== -1) errmsg = '请检查网络连接！'
+    else if (message.indexOf('timeout') !== -1) errmsg = '网络不稳定，连接超时！'
+    msg.error(errmsg);
+    return new Promise(() => { });
+  }
+)
 
 export default class Login extends Component {
   // 表单提交的回调
-  onFinish = values => {
-      // console.log('Received values of form: ', values);
-      const {username,password} = values;
-      axios.post('http://localhost:3000/login',{username,password}).then(
-        response => {console.log(response);},
-        error => {console.log(error);}
-      )
+  onFinish = async values => {
+    // console.log('Received values of form: ', values);
+    // 获取表单数据
+    // 如果表单收集的输入框的name属性的值与后台所要的参数(key)是一致的，那就可以直接写对象，不用结构对象也可
+    // const {username,password} = values;
+    let result = await axios.post('http://localhost:3000/login', values)
+    console.log("result:",result);
+    // axios发送post请求，默认会把参数通过请求体携带，那是以什么编码格式进行编码呢（urlencoded or json）？
   };
 
   /* 
@@ -24,15 +62,15 @@ export default class Login extends Component {
       报错：不输入直接点登录时会报values是undefined。什么都不输入，不会触发onFinish方法（该方法要校验通过之后才ok）。
       解决：设置形参默认值
   */
-  pwdValidator = (_,values='')=>{
+  pwdValidator = (_, values = '') => {
     // 定义在回调函数中，避免实时验证失败的信息一直push到数组中
     let errmsg = [];
-    if(!values.trim()) errmsg.push('Please input your Password !');
-    if(values.length < 4) errmsg.push('Must be greater than or equal to 4 digits !');
-    if(values.length > 12) errmsg.push('Must be less than or equal to 12 digits !');
+    if (!values.trim()) errmsg.push('Please input your Password !');
+    if (values.length < 4) errmsg.push('Must be greater than or equal to 4 digits !');
+    if (values.length > 12) errmsg.push('Must be less than or equal to 12 digits !');
     // 正则的test()验证value时候满足某个正则
-    if(!(/^\w+$/).test(values)) errmsg.push('Only contain English、Number or _ !');
-    if(errmsg.length > 0) return Promise.reject(errmsg);
+    if (!(/^\w+$/).test(values)) errmsg.push('Only contain English、Number or _ !');
+    if (errmsg.length > 0) return Promise.reject(errmsg);
     else return Promise.resolve();
   }
   render() {
@@ -46,31 +84,31 @@ export default class Login extends Component {
     return (
       <div className="login">
         <div className="login-header">
-          <img src={logo} alt=""/>
+          <img src={logo} alt="" />
           <h1>Commodity Management System</h1>
         </div>
         <div className="login-content">
-					<h1>W e l c o m e </h1>
+          <h1>W e l c o m e </h1>
           {/* 这儿放置antd组件。因为要自己用原生去实现一个实时的校验+交互效果 还是挺麻烦的。 */}
           <Form
             className="login-form"
             onFinish={this.onFinish}
           >
-            <Item 
+            <Item
               name="username"
               rules={[
-                {require:true,message:'Please input your Username!'},
-                {min:4,message:'Must be greater than or equal to 4 digits!'},
-                {max:12,message:'Must be less than or equal to 12 digits!'},
-                {pattern:/^\w+$/,message:'Only contain English、Number or _!'}
+                { require: true, message: 'Please input your Username!' },
+                { min: 4, message: 'Must be greater than or equal to 4 digits!' },
+                { max: 12, message: 'Must be less than or equal to 12 digits!' },
+                { pattern: /^\w+$/, message: 'Only contain English、Number or _!' }
               ]}
             >
               <Input prefix={<UserOutlined className="site-form-it em-icon" />} placeholder="Username" />
             </Item>
-            <Item 
+            <Item
               name="password"
               rules={[
-                {validator:this.pwdValidator}
+                { validator: this.pwdValidator }
               ]}
             >
               <Input
