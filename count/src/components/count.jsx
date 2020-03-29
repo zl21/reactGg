cb29974292d7210299a8f33fdee008f0e972bd9d
zl21 <44433851+zl21@users.noreply.github.com>
2020-03-29 17:43:48 +0800

    :count_v1.0
    纯react版。

---
+++ count/src/components/count.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+export default class Count extends Component {
+  state = { count: 0 }
+
+  // increment:
+  increment = () => {
+    // 获取用户选择的数字
+    let { value } = this.refs.checkNumber;
+    // Update state
+    let { count } = this.state;
+    this.setState({ count: count + value * 1 });
+  }
+  // decrement:
+  decrement = () => {
+    let { value } = this.refs.checkNumber;
+    let { count } = this.state;
+    this.setState({ count: count - value * 1 });
+  }
+  // incrementIfOdd:
+  incrementIfOdd = () => {
+    let { value } = this.refs.checkNumber;
+    let { count } = this.state;
+    if (count % 2 === 1) {
+      this.setState({ count: count + value * 1 });
+    }
+  }
+  // incrementAsync:
+  incrementAsync = () => {
+    let { value } = this.refs.checkNumber;
+    let { count } = this.state;
+    setTimeout(() => {
+      this.setState({ count: count + value * 1 });
+    },1000);
+  }
+  render() {
+    return (
+      <div>
+        <h2>result：{this.state.count}</h2>
+        <select ref="checkNumber">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>&nbsp;
+        <button onClick={this.increment}>+</button>&nbsp;
+        <button onClick={this.decrement}>-</button>&nbsp;
+        <button onClick={this.incrementIfOdd}>increment if odd</button>&nbsp;
+        <button onClick={this.incrementAsync}>increment async</button>
+      </div>
+    )
+  }
+}
